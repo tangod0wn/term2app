7886f4231262565d1b8fa89cfbc98306eec9cd91
Neil Anderson <neil@xmap.com.au>
2016-06-24 10:09:01 +1000

    finished users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def site_title_text
-    ENV["SITE_TITLE"] || "RRHNClone."
+    ENV["SITE_TITLE"] || "OZSEC"
   end
 
   def seo_title_text
-    ENV["SEO_TITLE"] || "Rails Hackernews Reddit Producthunt Clone Github"
+    ENV["SEO_TITLE"] || "Cyber Security"
   end
 
   def link_to_list_item(name, link, options={})
